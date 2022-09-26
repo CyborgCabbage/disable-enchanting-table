d0a51cff13348e749237277d421fa6f766cf63d1
FinlaySML <2100719@uad.ac.uk>
2022-09-26 10:03:39 +0100

    Everything

---
+++ src/main/java/cyborgcabbage/noenchant/mixin/DisableEnchantingTableMixin.java
@@ -0,0 +1,30 @@
+package cyborgcabbage.noenchant.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.EnchantingTableBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EnchantingTableBlock.class)
+public class DisableEnchantingTableMixin {
+	@Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
+	private void redirect(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+		if(!world.isClient) {
+			player.sendMessage(new TranslatableText("noenchant.warning").formatted(Formatting.RED), true);
+			cir.setReturnValue(ActionResult.CONSUME);
+		}else{
+			cir.setReturnValue(ActionResult.SUCCESS);
+		}
+	}
+}
